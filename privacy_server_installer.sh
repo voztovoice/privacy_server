23b022a3ef94b900fc226bfb7fbc6ea860e49f97
Mesa Proyectos SAS <2766135+voztovoice@users.noreply.github.com>
2026-01-09 17:09:19 -0500

    Update privacy_server_installer.sh

--- privacy_server_installer.sh
+++ privacy_server_installer.sh
@@ -237,7 +237,7 @@ dnf update -y
 log_info "Instalando paquetes base..."
 dnf install -y \
     nano wget git curl \
-    chrony nftables \
+    chrony nftables epel-release \
     iptables-services \
     policycoreutils-python-utils \
     openssl tar unzip sqlite \
